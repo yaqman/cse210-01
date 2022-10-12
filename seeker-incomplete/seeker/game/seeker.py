377e659169c9664af92f72883ddac80de8307f08
yaqman <kokiyaq57@byui.edu>
2022-10-13 00:24:47 +0900

    week5(3)

--- seeker-incomplete/seeker/game/seeker.py
+++ seeker-incomplete/seeker/game/seeker.py
@@ -1,13 +1,11 @@
 import random
 
-def __init__(self):
-    self._location = random.randint(1, 1000)
-
- 
-
-
-def get_location(self):
-    return self._location
- 
-def move_location(self, location):
-    self._location = location
+class Seeker:
+    def __init__(self):
+        self._location = random.randint(1, 1000)
+        
+    def get_location(self):
+        return self._location
+        
+    def move_location(self, location):
+        self._location = location
