b047721becc44cc7af6a8847320f0ed034f28f13
yaqman <kokiyaq57@byui.edu>
2022-10-13 00:37:45 +0900

    week5(3)

--- seeker-incomplete/seeker/game/seeker.py
+++ seeker-incomplete/seeker/game/seeker.py
@@ -2,10 +2,27 @@ import random
 
 class Seeker:
     def __init__(self):
-        self._location = random.randint(1, 1000)
+         """Constructs a new Seeker.
+
+        Args:
+            self (Seeker): An instance of Seeker.
+        """
+
+         self._location = random.randint(1, 1000)
         
     def get_location(self):
+        """Gets the current location.
+        
+        Returns:
+            number: The current location,
+        """
         return self._location
         
     def move_location(self, location):
+        """Moves to the given location.
+
+        Args:
+            self (Seeker): An instance of Seeker.
+            location (int): The given location.
+        """
         self._location = location
