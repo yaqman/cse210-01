b5dadce8e281e036ee67d809e8be26393d174772
yaqman <kokiyaq57@byui.edu>
2022-11-09 01:00:02 +0900

    week9

---
+++ snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -0,0 +1,9 @@
+from game.scripting.action import Action
+
+
+# TODO: Implement MoveActorsAction class here! 
+
+# Override the execute(cast, script) method as follows:
+# 1) get all the actors from the cast
+# 2) loop through the actors
+# 3) call the move_next() method on each actor
